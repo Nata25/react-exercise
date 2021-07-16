85ae283645ae7db59964db12cc14498aa03b5be3
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-07-16 15:07:57 +0300

    More material ui components and makeStyles hook

--- src/components/DataDisplay.jsx
+++ src/components/DataDisplay.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Fetcher from './Fetcher'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
 
 const DataDisplay = () => {
   const [data, setData] = React.useState(() => [])
@@ -21,17 +23,25 @@ const DataDisplay = () => {
           throw new Error(e)
         })
   }, [activeUrl])
+
+  const useStyles = makeStyles({
+    list: {
+      listStyleType: 'none',
+      paddingLeft: 0
+    }
+  })
+  const styles = useStyles()
   
   return (
-    <div>
+    <Container fluid>
       <h1>Fetch some data!</h1>
       <Fetcher dataAlias={activeUrl} submitUrl={setUrl}/>
-      <ul>
+      <ul className={styles.list}>
         {data.map(user => 
           <li key={user.id}>{JSON.stringify(user)}</li>
         )}        
       </ul>
-    </div>
+    </Container>
   )
 }
 
--- src/components/Fetcher.jsx
+++ src/components/Fetcher.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
 
 const Fetcher = ({ dataAlias, submitUrl }) => {
   const urls = ['users', 'posts', 'comments']
   return (
-    <div>
+    <Grid container spacing={2} justifyContent="center" alignItems="center">
       {urls.map(button => {
         return (
-          <Button
-            key={button}
-            onClick={() => submitUrl(button)}
-            color={dataAlias === button ? 'primary' : 'default'}
-            variant="contained"
-          >
-            {button}
-          </Button>
+          <Grid item key={button}>
+            <Button
+              onClick={() => submitUrl(button)}
+              color={dataAlias === button ? 'primary' : 'default'}
+              variant="contained"
+            >
+              {button}
+            </Button>  
+          </Grid>
         )
         })
       }
-    </div>
+    </Grid>
   )
 }
 
